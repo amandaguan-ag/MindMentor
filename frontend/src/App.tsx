7828284e24a56f11e8047fe98b1814308314e17a
Amanda Guan <ag.amandaguan@gmail.com>
2023-09-09 14:09:20 -0700

    add background and center the content

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,7 +2,7 @@ import Controller from "./components/Controller";
 
 function App() {
   return (
-    <div className="">
+    <div className="bg-gray-100 min-h-screen flex justify-center items-center p-4">
       <Controller />
     </div>
   );
